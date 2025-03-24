4859ec04c3aaca87602d9cbc1838ac67dcb5c779
baorun li <brbjarneli@gmail.com>
2025-03-24 15:17:13 +0800

    add Fevo__check_empty

--- include/common.hpp
+++ include/common.hpp
@@ -1,8 +1,18 @@
-
+#include <stdio.h>
 #define Fevo_check_eq(e1, e2)\
 {\
   if(e1 != e2) {\
-    printf("ERROR: %s:%d,",__FILE__,__LINE__);\
+    printf("ERROR: %s:%d, ",__FILE__,__LINE__);\
+    fflush(stdout);\
     throw std::runtime_error("Fevo_check_eq failed: " #e1 " != " #e2);\
+  }\
+}
+
+#define Fevo_check_empty(e)\
+{\
+  if(e.empty()) {\
+    printf("ERROR: %s:%d, ",__FILE__,__LINE__);\
+    fflush(stdout);\
+    throw std::runtime_error("Fevo_check_empty failed: " #e " is empty");\
   }\
 }
